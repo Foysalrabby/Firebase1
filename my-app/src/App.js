53d02d4d4647fa078361140f6acf96d31e812955
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2023-08-06 02:04:53 +0600

    work login with fairbase

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,108 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
-import { initializeApp } from 'firebase/app';
+import firebase, {initializeApp} from './fairbase';
+import { getAuth} from 'firebase/auth';
+import { signInWithPopup , GoogleAuthProvider} from "firebase/auth";
+import {  signOut } from "firebase/auth";
+const provider = new GoogleAuthProvider();
+
+
+
+
 
 function App() {
+  const [userinfo,setinfo]=useState({
+    isSigned: false,
+    name:"",
+    email:"",
+    photo:""
+
+  });
+  const handleuser=()=>{
+
+    const auth = getAuth();
+    signInWithPopup(auth,provider)
+    .then(res =>{
+      const {displayName,email,photoUrl}=res.user;
+      const userdata = {
+           isSigned:true,
+           name : displayName,
+           email : email,
+           photo :photoUrl
+
+
+      }
+      setinfo(userdata);
+      
+      console.log(displayName,email,photoUrl);
+    }).catch(error =>{
+      console.log(error.message);
+    })
+  
+  };
+
+  const handlesignout=()=>{
+
+    const auth = getAuth();
+    signOut(auth)
+    .then(res => {
+      const sinoutuser={
+       isSigned:false,
+       name:'',
+       email:'',
+       photo:''
+        
+      }
+   setinfo(sinoutuser);
+   console.log(res);
+
+    }).catch(error=>{
+      console.log(error);
+
+    });
+      
+    console.log("clicked");
+
+  }
+  //----------------handlesubmit
+
+  const handlesubmit = () => {
+
+
+  }
+//-------------onChange={handlechange} 
+
+const handlechange = () => {
+
+
+}
+
+
+
   return (
     <div className="App">
+    {
+     userinfo.isSigned ?<button onClick={handlesignout}>singnout</button>:
+     <button onClick={handleuser}>Sign in</button> //like if (userinfo.isSigned == true)
+     //<button onClick={handlesignout}>singnout</button>:
+    }
      
+     {
+      userinfo.isSigned && <div>
+      <p>{userinfo.name}</p>
+      <p>{userinfo.email}</p>
+      <img src={userinfo.photo} alt="m" srcset="" />
+      </div>
+     }
+     <h1> Your authentication...</h1>
+       <form onSubmit={handlesubmit}>
+        <input type="text" name="name" onChange={handlechange} placeholder="Enter your name" required /><br></br>
+        <input type="password" name="password" onChange={handlechange}  placeholder="Enter your password" required/><br/>
+        <input type="submit" value="submit" />
+
+       </form>
+
     </div>
   );
 }
--- my-app/src/fairbase.js
+++ my-app/src/fairbase.js
@@ -1,10 +1,11 @@
-const firebaseConfig = {
+import { initializeApp } from 'firebase/app';
+const firebase = initializeApp ({
     apiKey: "AIzaSyAvfaoaizz3puorqEvkHRi2YZg6xaJIdoM",
     authDomain: "emajon-project-fe2f2.firebaseapp.com",
     projectId: "emajon-project-fe2f2",
     storageBucket: "emajon-project-fe2f2.appspot.com",
     messagingSenderId: "1072967424625",
     appId: "1:1072967424625:web:19098b947e2075953c59d0"
-  };
+  });
 
-export default firebaseConfig;
+export default firebase;
